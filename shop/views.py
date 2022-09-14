a92571c6353c717c3c438929cfc2028741b30b6e
aleksej <xfrbt1@gmail.com>
2022-09-14 20:13:04 +0300

    Initial commit

--- shop/views.py
+++ shop/views.py
@@ -50,8 +50,7 @@ class CartDeleteItem(DeleteView):
         qs = super().get_queryset()
         qs.filter(order__user=self.request.user)
         return qs
-<<<<<<< HEAD
-=======
+
 
 
 @login_required(login_url=reverse_lazy('login'))
@@ -59,4 +58,4 @@ def make_order(request):
     cart = Order.get_cart(request.user)
     cart.make_order()
     return redirect('shop')
->>>>>>> 800faf1 (Initial commit)
+
